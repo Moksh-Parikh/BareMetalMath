2d72bc94051e874abb3c0eecd86aa30b0e772696
Moksh Parikh <mokshparikh30@gmail.com>
2025-07-03 19:42:58 +0800

    squareRoot function works. Added the calculatePercentage function. Uncommented printing floating-points support in the Makefile

--- AVR/main.c
+++ AVR/main.c
@@ -7,27 +7,7 @@
 #include "headers/USART.h"
 #include "headers/pinDefines.h"
 
-/* ISR(TIMER1_COMPA_vect) { */
-/*     PORTB ^= (1 << PB5); */
-/*     printString("toggled!\r\n"); */
-/* } */
-
-
-/* static inline void initTimer1() { */
-/*     TCCR1A = 0; */
-/*     TCCR1B = 0; */
-/*     // 16 000 000 (16 million) / 1024 (clock prescaler set below) */
-/*     // = 15624 ticks per second */
-/*     OCR1A = 15624; */
-
-/*     // put the clock in ctc mode and set the prescaler to 1024 */
-/*     TCCR1B |= (1 << CS10) | (1 << CS12) | (1 << WGM12); */
-
-/*     TIMSK1 |= (1 << OCIE1A); */
-
-/*     sei(); */
-/* } */
-
+// TODO: add support for fractional indices
 float exponent(float number, int power) {
     float returnVal = number;
     for (int i = 0; i < power - 1; i++) {
@@ -37,68 +17,46 @@ float exponent(float number, int power) {
     return returnVal;
 }
 
-float squareRoot(float square) {
-    float guessArray[6];
+float calculatePercent(float numerator, float denominator) {
+    return (100 * numerator) / denominator;
+}
 
-    printString("in squareRoot\r\n");
+
+// uses Heron's Method to calculate the square root
+// https://gregorygundersen.com/blog/2023/02/01/estimating-square-roots/
+float squareRoot(float square, int accuracy) {
+    float guessArray[accuracy];
 
     for (int j = 1; j < square; j++) {
         float iteratorSquared = exponent(j, 2);
-
-        printString("j and j^2:\r\n");
-        printByte(j);
-        transmitByte('\r');
-        transmitByte('\n');
-        
-        printByte(iteratorSquared);
-        transmitByte('\r');
-        transmitByte('\n');
         
         if (iteratorSquared == square) { return (float)j; }
         
-        if ( iteratorSquared - square < 3 || iteratorSquared - square > -3 ) {
+        if ( calculatePercent(iteratorSquared, square) > 97.0) {// || iteratorSquared - square > -3 ) {
             guessArray[0] = (float)j;
             break;
         }
     }
 
-
-    printByte(guessArray[0]);
-    printString("\r\n");
-
-    for(int i = 1; i < 6; i++) {
+    for(int i = 1; i < accuracy; i++) {
         float correctionFactor = square / guessArray[i - 1];
 
         guessArray[i] = (guessArray[i - 1] + correctionFactor) / 2;
-        /* guessArray[i] = (1 / 2) * (guessArray[i - 1] + (square / guessArray[i - 1]) ); */
-
-        printString("guess\r\n");
-        for (int k = 0; k < 4; k++) {
-            printBinaryByte( *( &guessArray[i] + k) );
-        }
-
-        printString("\r\n");
     }
 
-    return guessArray[5];
+    return guessArray[accuracy - 1];
 }
 
 
 int main(void) {
-    DDRB |= (1 << PB5);
     initUSART();
-    /* initTimer1(); */
     printString("Initialised!\r\n");
     
     char buffer[100];
-    float result = squareRoot(36);
+    float result = squareRoot(255, 6);
+    snprintf(buffer, 100, "Estimated square root of %d: %f\r\n", 255, result);
 
-    snprintf(buffer, 100, "Hi: %05f\r\n", result);
-
-    printString("\r\nSquare Root:\r\n");
     printString(buffer);
-    printString("\r\n");
-    /* while (1) {} */
 
     return(0);
 }
