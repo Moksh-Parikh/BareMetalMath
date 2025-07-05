d4b791ae1cefc8d3c5ff6d894033aa84aa3f1732
Moksh Parikh <mokshparikh30@gmail.com>
2025-07-05 18:14:36 +0800

    Added CORDIC implementation. Currently only supports radians up to pi / 2

--- AVR/main.c
+++ AVR/main.c
@@ -7,9 +7,19 @@
 #include "headers/USART.h"
 #include "headers/pinDefines.h"
 
+float exponent(float number, int power);
+float calculatePercent(float numerator, float denominator);
+float squareRoot(float square, int accuracy);
+
 // TODO: add support for fractional indices
 float exponent(float number, int power) {
     float returnVal = number;
+    
+    if (power == 0) {return 1.0;}
+    else if (power < 0) {
+        returnVal = 1 / exponent(number, power * -1);
+    }
+
     for (int i = 0; i < power - 1; i++) {
         returnVal *= number;
     }
@@ -47,16 +57,76 @@ float squareRoot(float square, int accuracy) {
     return guessArray[accuracy - 1];
 }
 
+/*
+    * x = Xin * cos(angle) - Yin * sin(angle)
+    * y = Xin * sin(angle) + Yin * cos(angle)
+    * if we set Xin to 1 and Yin to 0,
+    * then the equations become:
+    * x = cos(angle)
+    * y = sin(angle)
+    *
+*/
+
+// translated from Python implementation at:
+//      https://en.wikipedia.org/wiki/CORDIC
+float CORDIC(float alpha, float* sin, float* cos) {
+    float thetaTable[16] = {0.785398, 0.463647, 0.244978, 0.124354, 0.062418, 0.031239, 0.015623, 0.007812, 0.003906, 0.001953, 0.000976, 0.000488, 0.000244, 0.000122, 0.000061, 0.000030};
+
+    /* char buffer[500]; */
+
+    float correctionFactor = 0.607259;
+    float theta = 0.0;
+    float x = 1.0;
+    float y = 0.0;
+    float exponent2to1 = 1;
+
+    int rotationDirection;
+
+    float tempX, tempY; 
+
+    /* snprintf(buffer, 500, "%f\r\n", correctionFactor); */
+    /* printString(buffer); */
+
+    for (int i = 0; i < 16; i++) {
+        rotationDirection = theta < alpha ? 1 : -1;
+        theta += rotationDirection * thetaTable[i];
+
+        tempX = x - rotationDirection * y * exponent2to1;
+        tempY = y + rotationDirection * x * exponent2to1;
+
+        x = tempX;
+        y = tempY;
+
+        /* snprintf(buffer, 500, "\e[38:5:23mSigma: %d, theta: %f\r\n\e[38:5:225mxOut: %f, yOut: %f\r\n\e[38:5:130mtan(): %f\r\n", rotationDirection, theta, x, y, y/x); */
+        /* printString(buffer); */
+
+        exponent2to1 /= 2;
+    }
+    
+    x *= correctionFactor;
+    y *= correctionFactor;
+
+    /* snprintf(buffer, 500, "%f / %f = %f\r\n", x, y, y/x); */
+    /* printString(buffer); */
+    
+    *cos = x;
+    *sin = y;
+
+    return y / x;
+}
 
 int main(void) {
     initUSART();
     printString("Initialised!\r\n");
-    
-    char buffer[100];
-    float result = squareRoot(255, 6);
-    snprintf(buffer, 100, "Estimated square root of %d: %f\r\n", 255, result);
+ 
+    /* for (int i = 0; i <= 90; i++) { */
+        char buffer[100];
+        float cosine, sine;
+        float tangent = CORDIC(1.570796, &cosine, &sine); // squareRoot(255, 6);
+        snprintf(buffer, 100, "Estimated sine, cosine and tangent of %d: %f, %f, %f\r\n", 1.570796, sine, cosine, tangent);
 
-    printString(buffer);
+        printString(buffer);
+    /* } */
 
     return(0);
 }
